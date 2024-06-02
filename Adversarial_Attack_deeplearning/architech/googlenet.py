b01cb78723aa6b8c0f7680f4c4b4fc322b3fc60b
khoa16122004 <trannhatkhoacm1612@gmail.com>
2024-06-02 13:57:51 +0700

    "update architech"

---
+++ Adversarial_Attack_deeplearning/architech/googlenet.py
@@ -0,0 +1,34 @@
+'''GoogLeNet with PyTorch.'''
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class MnistResNet101(nn.Module):
+    def __init__(self, in_channels=1):
+        super(MnistResNet101, self).__init__()
+
+        # Load a pretrained resnet model from torchvision.models in Pytorch
+        self.model = models.resnet101(pretrained=True)
+
+        # Change the input layer to take Grayscale image, instead of RGB images.
+        # Hence in_channels is set as 1 or 3 respectively
+        # original definition of the first layer on the ResNet class
+        # self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.model.conv1 = nn.Conv2d(in_channels, 64, kernel_size=7, stride=2, padding=3, bias=False)
+
+        # Change the output layer to output 10 classes instead of 1000 classes
+        num_ftrs = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_ftrs, 10)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+def test():
+    net = MnistResNet101()
+    x = torch.randn(1,3,32,32)
+    y = net(x)
+    print(y.size())
+
+test()
